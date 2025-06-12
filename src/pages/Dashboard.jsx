c252b25f1bcb2c7fc0c90aeb5c116cafb7faeffb
kite1412 <frozradit@gmail.com>
2025-06-12 10:59:02 +0700

    feat: integrate dashboard api

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,12 +2,29 @@ import { useEffect, useState } from "react";
 import ChartPeminjamanBuku from "../components/ChartPeminjamanBuku";
 import dataPeminjamanChart from "../mocks/dataPeminjamanChart";
 import PengunjungService from "../services/PengunjungService";
+import MemberService from "../services/MemberService";
+import BookService from "../services/BookService";
+import TransactionService from "../services/TransactionService";
 
 function Dashboard() {
   const [totalPengunjung, setTotalPengunjung] = useState(0);
+  const [totalMember, setTotalMember] = useState(0);
+  const [totalBuku, setTotalBuku] = useState(0);
+  const [totalBukuTersedia, setTotalBukuTersedia] = useState(0);
+  const [totalBukuDipinjam, setTotalBukuDipinjam] = useState(0);
 
   useEffect(() => {
-    PengunjungService.getPengunjung().then(r => setTotalPengunjung(r.total));
+    PengunjungService.getPengunjung().then(r => {
+      setTotalPengunjung(r.total);
+      MemberService.getMembers().then(r => setTotalMember(r.length));
+      BookService.getBooks().then(r => {
+        setTotalBuku(r.length);
+        TransactionService.getTransactions().then(res => {
+          setTotalBukuTersedia(r.length - res.length);
+          setTotalBukuDipinjam(res.length);
+        });
+      });
+    });
   }, []);
 
   return (
@@ -17,19 +34,19 @@ function Dashboard() {
       <div className="w-full flex text-md font-semibold gap-5 justify-between">
         <div className="flex flex-col border-l-4 border-blue-500 rounded-sm shadow-md px-10 py-10 w-full">
           <span>Total Buku</span>
-          <span>10</span>
+          <span>{totalBuku}</span>
         </div>
         <div className="flex flex-col border-l-4 border-blue-500 rounded-sm shadow-md px-10 py-10 w-full">
           <span>Buku Tersedia</span>
-          <span>5</span>
+          <span>{totalBukuTersedia}</span>
         </div>
         <div className="flex flex-col border-l-4 border-blue-500 rounded-sm shadow-md px-10 py-10 w-full">
           <span>Buku Dipinjam</span>
-          <span>5</span>
+          <span>{totalBukuDipinjam}</span>
         </div>
         <div className="flex flex-col border-l-4 border-blue-500 rounded-sm shadow-md px-10 py-10 w-full">
           <span>Total Member</span>
-          <span>9</span>
+          <span>{totalMember}</span>
         </div>
         <div className="flex flex-col border-l-4 border-blue-500 rounded-sm shadow-md px-10 py-10 w-full">
           <span>Pengunjung Hari Ini</span>
